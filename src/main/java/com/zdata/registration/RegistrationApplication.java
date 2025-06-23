38376e4d3b21e553152646e7a0e5552dde9d7089
Janith-viranga <128822354+Janith-01@users.noreply.github.com>
2025-06-23 14:29:43 +0530

    update the main application

--- src/main/java/com/zdata/registration/RegistrationApplication.java
+++ src/main/java/com/zdata/registration/RegistrationApplication.java
@@ -1,4 +1,4 @@
-package com.zdata.registration.registration;
+package com.zdata.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
